ebd27e02065021789e9c5213062e890664352d17
Michael Hu <43020369+GuessHuIAm@users.noreply.github.com>
2023-02-10 16:09:35 -0500

    added basic communication framework
    no wire protocol yet though

--- client.py
+++ client.py
@@ -1,34 +1,88 @@
 import socket
+import threading
 
-user = input("Welcome to <Name TBD>! Please enter your username: ")
 
-# Check whether username alr in use by making a request to server, in which case ask user for password
-# Otherwise create new account, request for password
+def send_messages(client):
+    """ Send and receive messages to and from the server and print them to the console """
+    user = login(client)
+    
+    task = None
+    while (task != 'q'):
+        task = input('''Please select a task by typing in a letter:
+            a -- View all users
+            m -- Send new message
+            d -- Send undelivered messages in your queue
+            q -- Quit/Log out
+            ''')
+        
+        # TODO: Implement functionality for each task.
+        if task == 'a':
+            pass
+        elif task == 'm':
+            message = input("What message would you like to send?")
+            recipient = input("Who would you like to send it to?")
+            client.send(f"send {recipient} {message}".encode())
+        elif task == 'd':
+            pass
 
-user_exists = False
-if (user_exists):
-    password = input(f"Welcome back, {user}! Please enter your password: ")
 
-    # TODO: Implement password verification procedure.
-
-else:
-    password = input(f"Welcome, {user}! Seems like you're new here! To register, please enter a password: ")
-    confirm = input("Please confirm your password: ")
-    while (password != confirm):
-        password = input("Hmm, seems like your passwords didn't match. Please re-enter your desired password: ")
+def login(client):
+    """ Login the client or create a new account"""
+    
+    # Asks the server if the username is already in use
+    user = input("Please enter your username: ")
+    client.send(user.encode())
+   
+    response = client.recv(1024).decode()
+    
+    # If the user is already in use, ask for password and check if it matches
+    if response == "exists":
+        password = input(f"Welcome back, {user}! Please enter your password: ")
+        
+        client.send(password.encode())
+        response = client.recv(1024).decode()
+    
+        while response == "error":
+            password = input("Hmm, seems like your password was incorrect. Please re-enter your password: ")
+            client.send(password.encode())
+            response = client.recv(1024).decode()
+            
+        if response == "success":
+            print("Successfully logged in!")
+    else:
+        # If the user is already in use, ask for password and check if it matches
+        password = input(f"Welcome, {user}! Seems like you're new here! To register, please enter a password: ")
         confirm = input("Please confirm your password: ")
 
-    # TODO: Send user and password information to server.
+        # If the passwords don't match, ask for password again
+        while (password != confirm):
+            password = input("Hmm, seems like your passwords didn't match. Please re-enter your desired password: ")
+            confirm = input("Please confirm your password: ")
+    
+        client.send(password.encode())
+        response = client.recv(1024).decode()
 
-# TODO: Connect to server.
+        if response == "success":
+            print("Successfully registered and logged in!")
+            
+    return user
 
-task = None
-while (task != 'q'):
-    task = input('''Please select a task by typing in a letter:
-    a -- View all users
-    m -- Send new message
-    d -- Send undelivered messages in your queue
-    q -- Quit/Log out\n''')
 
-    # Implement functionality for each task. Prob should use dict that maps task to function.
+def start_client():
+    """ Start the client and connect to the server """
+    
+    host = "localhost"
+    port = 8000
 
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect((host, port))
+
+    # TODO: Come up with a badass name
+    print("Welcome to <Name TBD>!")
+
+    receive_thread = threading.Thread(target=send_messages, args=([client]))
+    receive_thread.start()
+
+
+if __name__ == "__main__":
+    start_client()
--- server.py
+++ server.py
@@ -1,5 +1,123 @@
 import socket
+import hashlib
+import threading
 
-users = {}
 
-# TODO: Start up  server.
+# Dictionary to store the users and their messages
+accounts = {}
+messages = {}
+
+# Currently connected clients
+clients = set()
+
+def receive_message(client, address):
+    """Receive messages from the client and handles them accordingly"""
+    
+    user = login(client)
+
+    while True:
+        data = client.recv(1024).decode()
+        if data == "":
+            break
+         # Split the data into command and message
+        command, *message = data.split(" ")
+
+        # If the command is "login"
+        if command == "login":
+            # Check if the username is in the existing accounts
+            if message[0] in accounts:
+                client.send("Error: User already logged in".encode())
+        else:
+            print(f"Unknown command: {data}")
+
+
+def login(client):
+    """Login the user and return the username"""
+    username = client.recv(1024).decode()
+
+    if username in accounts:
+        # If the username exists, ask for password
+        client.send("exists".encode())
+        
+        # Hash the entered password and check it against the stored password hash
+        password = client.recv(1024).decode()
+        password_hash = hashlib.sha256(password.encode()).hexdigest()
+        
+        while accounts[username] != password_hash:
+            client.send("error".encode())
+            password = client.recv(1024).decode()
+            password_hash = hashlib.sha256(password.encode()).hexdigest()
+        
+        # TODO: Set up some sort of failure mechanism for when the password is
+        # incorrect too many times or user stops wanting to try.
+        
+    else:
+        # If the username doesn't exist
+        client.send("new".encode())
+        
+        # Hash the entered password and check it against the stored password hash
+        password = client.recv(1024).decode()
+        password_hash = hashlib.sha256(password.encode()).hexdigest()
+        accounts[username] = password_hash
+        
+    # Send a success message to the client
+    client.send("success".encode())
+    print(f"{username} has joined the chat")
+    clients.add(username)
+    return username
+
+
+def handle_client(client, address):
+    """Handle the client connection"""
+    
+    username = login(client)
+    if not username:
+        client.close()
+        return
+
+    while True:
+        data = client.recv(1024).decode()
+        if data == "":
+            break
+        command, *args = data.split(" ")
+        if command == "list":
+            client.send(str(accounts.keys()).encode())
+        elif command == "send":
+            recipient, message = args
+            if recipient in accounts:
+                accounts[recipient].append((username, message))
+                client.send(f"Message sent to {recipient}".encode())
+            else:
+                client.send(f"{recipient} is not a user".encode())
+        elif command == "deliver":
+            messages = accounts.get(username, [])
+            for sender, message in messages:
+                client.send(f"From {sender}: {message}".encode())
+            accounts[username] = []
+            
+    del accounts[username]
+    print(f"{username} has left the chat")
+    client.close()
+
+
+def start_server():
+    """Start the server"""
+    
+    host = "localhost"
+    port = 8000
+
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind((host, port))
+    server.listen()
+    print("Server started on", host, "port", port)
+
+    while True:
+        client, address = server.accept()
+        print("Accepted connection from", address)
+        client_thread = threading.Thread(
+            target=handle_client, args=(client, address))
+        client_thread.start()
+
+
+if __name__ == "__main__":
+    start_server()
