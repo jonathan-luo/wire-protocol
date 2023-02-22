2641f56db6291748e6162f741e63f8c7ec1a8c5e
Matt Kiley <mdkiley@college.harvard.edu>
2023-02-22 11:46:40 -0500

    grpc works

--- grpc_chat/client.py
+++ grpc_chat/client.py
@@ -1,5 +1,122 @@
-from concurrent import futures
-
 import grpc
-import time
+import chat_pb2_grpc as pb2_grpc
+import chat_pb2 as pb2
+import threading
 
+class ChatClient(object):
+    def __init__(self, addr):
+        self.host = addr
+        self.server_port = 50051
+
+        self.channel = grpc.insecure_channel(
+            '{}:{}'.format(self.host, self.server_port))
+
+        self.stub = pb2_grpc.ChatStub(self.channel)
+
+    def create_account(self, username, password):
+        account = pb2.Account(username=username, password=password)
+        return self.stub.CreateAccount(account)
+
+    def delete_account(self, username, password):
+        account = pb2.Account(username=username, password=password)
+        return self.stub.DeleteAccount(account)
+
+    def login(self, username, password):
+        account = pb2.Account(username=username, password=password)
+        return self.stub.Login(account)
+
+    def logout(self, username):
+        account = pb2.Account(username=username, password="")
+        return self.stub.Logout(account)
+
+    def list_accounts(self, searchterm):
+        search_term = pb2.SearchTerm(searchterm=searchterm)
+        return self.stub.ListAccounts(search_term)
+
+    def send_message(self, destination, source, text):
+        message_info = pb2.MessageInfo(destination=destination, source=source, text=text)
+        return self.stub.SendMessage(message_info)
+    
+    def listen_messages(self, username):
+        account = pb2.Account(username=username)
+        messages = self.stub.ListenMessages(account)
+        for msg in messages:
+            print(msg.text)
+
+
+
+def login_ui(client):
+    username = input("What is your name?")
+    password = input("What is your password?")
+    result = client.create_account(username=username, password=password)
+    if result.error == False:
+        print(f'Welcome, {username}. I see this is your first time here.  Confirm your password below:')
+        err = True
+        while err == True:
+            password = input("Password: ")
+            result = client.login(username=username, password=password)
+            err = result.error
+            if err == True:
+                print(result.message)
+        print(result.message)
+    else:
+        result = client.login(username=username, password=password)
+        while result.error == True:
+            print(result.message)
+            username = input("Username: ")
+            password = input("Password: ")
+            result = client.login(username=username, password=password)
+        print(result.message)
+
+    return username, password
+
+if __name__ == '__main__':   
+    th = None
+    addr = input("Input IP Address of Host:")
+    client = ChatClient(addr)
+    username, password = login_ui(client)
+
+    # Once use has logged in, start thread listening for messages and printing at client
+    th = threading.Thread(target=client.listen_messages, args=(username,))
+    th.start()
+    
+    while (True):
+        user_input = input("list, send, logout, or delete?")
+        
+        if user_input == "list":
+            regex = input("Provide a regular expression:")
+            result = client.list_accounts(regex)
+            print(f'{result}')
+
+        elif user_input == "send":
+            destination = input("To:")
+            text = input("Message:")
+            result = client.send_message(destination=destination, source=username, text=text)
+            print(f'{result}')
+        
+        elif user_input == "logout":
+            confirmation = input("Are you sure you want to logout? (y/N)")
+            if confirmation == "y" or confirmation == "Y":
+                client.logout(username=username)
+                print("Successfully logged out")
+                username, password = login_ui(client)
+                th = threading.Thread(target=client.listen_messages, args=(username,))
+                th.start()
+            else:
+                pass
+
+        elif user_input == "delete":
+            password = input("Password:")
+            confirmation = input("Are you sure you want to logout? (y/N)")
+            if confirmation == "y" or confirmation == "Y":
+                result = client.delete_account(username=username, password=password)
+                if result.error == True:
+                    print(result.message)
+                else:
+                    print("Successfully deleted")
+                    username, password = login_ui(client)
+                    th = threading.Thread(target=client.listen_messages, args=(username,))
+                    th.start()
+
+        else:
+            print("Invalid")
---
+++ grpc_chat/server.py
@@ -0,0 +1,140 @@
+import grpc
+from concurrent import futures
+import time
+import chat_pb2_grpc as pb2_grpc
+import chat_pb2 as pb2
+import threading
+import re
+import socket
+
+# Associates a unique username with a password
+accounts = {}
+
+# Associates a username with a logged-in status
+accounts_status = {}
+
+# Associates a user with a dictionary storing senders : list of messages
+accounts_queue = {}
+
+class ChatService(pb2_grpc.ChatServicer):
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def CreateAccount(self, request, context):
+        username = request.username
+        password = request.password
+        if username not in accounts:
+            accounts[username] = password
+            accounts_status[username] = False
+            accounts_queue[username] = {}
+            result = f'{username} added'
+            response = {'message': result, 'error': False}
+        else:
+            result = "Error: Username already in use"
+            response = {'message': result, 'error': True}
+        
+        return pb2.ServerResponse(**response)
+
+    def DeleteAccount(self, request, context):
+        username = request.username
+        password = request.password
+        if username not in accounts:
+            result = f'{username} is not an existing username'
+            response = {'message': result, 'error': True}
+            return pb2.ServerResponse(**response)
+            
+        if password != accounts[username]:
+            result = f'Wrong password for {username}'
+            response = {'message': result, 'error': True}
+            return pb2.ServerResponse(**response)
+
+        del(accounts[username])
+        result = f'{username} deleted'
+        response = {'message': result, 'error': False}
+        return pb2.ServerResponse(**response)
+
+    # Once use logs in, server immediately creates a thread for that user that is working on user's behalf
+    # Looking for messages
+    def Login(self, request, context):
+        username = request.username
+        password = request.password
+        
+        if username not in accounts:
+            result = f'{username} is not a registered account.'
+            response = {'message': result, 'error': True}
+            return pb2.ServerResponse(**response)
+
+        if password != accounts[username]:
+            result = "Password does not match username"
+            response = {'message': result, 'error': True}
+            return pb2.ServerResponse(**response)
+            
+        accounts_status[username] = True
+        result = f'{username}, you are logged in'
+        response = {'message': result, 'error': False}
+        return pb2.ServerResponse(**response)
+
+    def Logout(self, request, context):
+        username = request.username
+        accounts_status[username] = False
+        result = f'{username}, you are logged out'
+        response = {'message': result, 'error': False}
+        return pb2.ServerResponse(**response)
+
+    def ListAccounts(self, request, context):
+        searchterm = request.searchterm
+        pattern = re.compile(searchterm)
+        accounts_str = ""
+        for account in accounts:
+            if pattern.search(account) != None:
+                accounts_str += account + " "
+        accounts_str = accounts_str[:-1]
+        response = {'usernames': accounts_str}
+        return pb2.Accounts(**response)
+
+    # Send Message puts message into the destination user's queue
+    def SendMessage(self, request, context):
+        destination = request.destination
+        source = request.source
+        text = request.text
+
+        if source not in accounts or accounts_status[source] == False:
+            result = "Error: username not valid or not logged in"
+            response = {'message': result, 'error': True}
+            return pb2.ServerResponse(**response)
+
+        if destination not in accounts:
+            result = "Error: destination not valid"
+            response = {'message': result, 'error': True}
+            return pb2.ServerResponse(**response)
+            
+        if source not in accounts_queue[destination]:
+            accounts_queue[destination][source] = [text]
+        else:
+            accounts_queue[destination][source].append(text)
+        result = "Message Sent"
+        response = {'message': result, 'error': False}
+        return pb2.ServerResponse(**response)
+
+    def ListenMessages(self, request, context):
+        username = request.username
+        while accounts_status[username] == True:
+            myDict = accounts_queue[username]
+            for sender in list(myDict):
+                for msg in myDict[sender]:
+                    response = {'destination': username, 'source': sender, 'text': msg}
+                    yield pb2.MessageInfo(**response)
+                    myDict[sender].remove(msg)
+
+
+def serve():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    pb2_grpc.add_ChatServicer_to_server(ChatService(), server)
+    server.add_insecure_port('[::]:50051')
+    server.start()
+    host = socket.gethostbyname(socket.gethostname())
+    print(f'server started on {host}')
+    server.wait_for_termination()
+
+if __name__ == '__main__':
+    serve()
